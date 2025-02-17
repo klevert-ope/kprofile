0625be724599b1056bfcc2f06aa370d6f582c2f6
Klevert Opee <81738300+klevert-ope@users.noreply.github.com>
2025-02-17 21:30:07 +0400

    standalone setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	output: "standalone",
+	productionBrowserSourceMaps: false,
+	reactProductionProfiling: true,
+	compress: true,
+};
 
 export default nextConfig;
